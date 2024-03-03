ab2b50505e3f6e5fe44b80a6e5192a59e069d3db
Superpeterl <nueafa.p@kkumail.com>
2024-03-03 20:16:15 +0700

    save to db

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def collect_strokes():
     return 'Pen strokes data received and processed successfully'
 if __name__ == '__main__':
     testdb(uri)
-    app.run(host='localhost', port=80, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
